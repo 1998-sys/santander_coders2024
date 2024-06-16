3b700da5c1bc8ae616f67671fd4e588c603abda5
Matheus <matheus.bandeira74@gmail.com>
2024-06-16 12:01:42 -0300

    função de checagem de um valor numérico

--- auxiliar.py
+++ auxiliar.py
@@ -1,1 +1,25 @@
 ### Funçoes auxiliares
+import time
+
+def valor_numerico(mensagem):
+    """
+    Solicita que o usuário digite um valor e verifica se é numérico (inteiro ou ponto flutuante).
+    Continua solicitando até que um valor válido seja fornecido.
+
+    Parâmetros:
+    mensagem (str): A mensagem que será exibida ao solicitar a entrada do usuário.
+
+    Retorna:
+    float ou int: O valor numérico digitado pelo usuário.
+    
+    """
+    while True:
+        numero = input(mensagem)
+        try:
+            numero = float(numero)
+            return numero
+            break
+
+        except:
+            print('Entrada Inválida digite um valor numérico')
+            time.sleep(3)
